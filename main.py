6ebc592289504c2725a68bd5e001dd614566189e
Threde <anphmvn75@gmail.com>
2022-11-07 12:21:01 +0700

    add file

---
+++ main.py
@@ -0,0 +1,12 @@
+from queue import Empty
+from tokenize import blank_re
+from turtle import clear, clearscreen
+
+
+name = input("Please  enter your name: ")
+addresses =input("Please enter your addresses: ")
+age = input("Please enter your age: ")
+print("--------------------------------------------")
+print("Name: ", name)
+print("Age: ", age)
+print("Address: ", addresses)
