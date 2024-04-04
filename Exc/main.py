d2cfceb5f06c24bdbf5a8428f2d1469b81787788
Threde <67854175+thredeisacoder@users.noreply.github.com>
2024-04-04 13:19:59 +0700

    Add files

---
+++ Exc/main.py
@@ -0,0 +1,8 @@
+
+name = input("Please  enter your name: ")
+addresses = input("Please enter your addresses: ")
+age = int(input("Please enter your age: "))
+print("--------------------------------------------")
+print("Name: ", name)
+print("Age: ", age)
+print("Address: ", addresses)
